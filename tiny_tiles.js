41b7d6d3d82f5bec6facc998e0c805a5e71a84e0
rokrupnik <rok7rupnik@gmail.com>
2018-03-31 20:00:09 +0200

    First working version!

--- tiny_tiles.js
+++ tiny_tiles.js
@@ -1,38 +1,57 @@
-var fs = require('fs'),
-    imagemin = require('image-min'),
+const fs = require('fs'),
+    imagemin = require('imagemin'),
+    imageminPngquant = require('imagemin-pngquant'),
     path = require('path');
 
-var SRC = './tiles/',
-    DST = './tiny_tiles_image-min/';
+const SRC = './test_tiles/',
+    DST = './tiny_tiles_8/';
 
-var walkSync = function (dir) {
+const walkSync = async (dir, level) => {
     // Create dst directories if they do not exist
-    var dstDir = dir.replace(SRC, DST);
+    const dstDir = dir.replace(SRC, DST);
     if (!fs.existsSync(dstDir)){
         fs.mkdirSync(dstDir);
     }
 
-    var files = fs.readdirSync(dir);
+    const files = fs.readdirSync(dir);
         
-    files.slice(0,3).forEach(function (file) {
-        if (fs.statSync(dir + file).isDirectory()) {
-            walkSync(dir + file + '/');
-        } else {
-            if (fs.existsSync(dstDir + file)){
-                console.log('Image allready optimised: ', dir + file);
-            } else {                
-                var src = fs.createReadStream(dir + file);
-                console.log(typeof src.path, src.path);
-                var ext = path.extname(src.path);
-                
-                src
-                    .pipe(imagemin({ ext: ext }))
-                    .pipe(fs.createWriteStream(dstDir + file));
-            }  
-        }
+    const dirContainsTiles = files.some(file => {
+        return path.extname(file).toLowerCase() === '.png';
     });
+
+    if (dirContainsTiles) {
+        // Check if all images have been optimised
+        const allFilesAreOptimised = files.every(function(file) {
+            return fs.existsSync(dstDir + file);
+       });
+       if (allFilesAreOptimised) {
+            console.log('Tiles allready optimised in folder: ', dir);
+       } else {
+            console.log('Start: ', dir);
+            console.time(dir);
+
+            await imagemin([dir + '*.png'], dstDir, {
+                plugins: [
+                    imageminPngquant({quality: '65-80'})
+                ]
+            });
+           
+            console.log('Stop: ', dir);
+            console.timeEnd(dir);
+       }
+    } else {
+        for(var i = 0; i < files.length; i+=1) {
+            if (fs.statSync(dir + files[i]).isDirectory()) {
+                await walkSync(dir + files[i] + '/', level + 1);
+            }
+        }
+    }
     
     return;
 };
 
-walkSync(SRC, 0);
+console.time('Total time');
+walkSync(SRC, 0)
+    .then(() => {
+        console.timeEnd('Total time');
+    });
